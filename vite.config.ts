481c64d0e4e51b8b1eba69e55dad860a1139054b
ItsPapo <papoo2005@hotmail.com>
2025-08-19 18:32:24 -0400

    fix: add prop-types and prebundle in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['prop-types'], // ensure prebundled in dev
+  },
   server: {
     host: '127.0.0.1',
     proxy: {
